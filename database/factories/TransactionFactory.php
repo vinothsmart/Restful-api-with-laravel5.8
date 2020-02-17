eb89363f10daf5ce103f713125c0104c0e81026e
vinothsmart <s.vinothkanna90@gmail.com>
2020-02-17 12:50:39 +0530

    factories file created

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(Model::class, function (Faker $faker) {
+    $seller = Seller::has('products')->get->random();
+    $buyer  = User::all()->except($seller->id)->random();
+
+    return [
+        'quantity' => $faker->numberBetween(1, 3),
+        'buyer_id' => $buyer->id,
+        'product_id' => $seller->products->random()->id,
+    ];
+});
