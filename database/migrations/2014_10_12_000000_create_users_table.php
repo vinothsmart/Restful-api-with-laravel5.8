e8a9dc8bb5b5a865e13eeac1c2e160ca9d17410b
vinothsmart <s.vinothkanna90@gmail.com>
2020-02-20 16:43:40 +0530

    columns updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,9 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('verified')->default(User::UNVERIFIED_USER);
+            $table->string('verification_token')->nullable();
+            $table->string('admin')->default(User::REGULAR_USER);
             $table->timestamps();
         });
     }
