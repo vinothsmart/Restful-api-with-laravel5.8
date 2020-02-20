37e399738c7b42767b11865f617b4e04b0dca617
vinothsmart <s.vinothkanna90@gmail.com>
2020-02-20 16:28:51 +0530

    Propeties

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const AVAILABLE_PRODUCT = 'available';
+    const UNAVAILABLE_PRODUCT = 'unavailable';
+
+    protected $fillbale = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function isAvailable()
+    {
+        return $this->status == Product::AVAILABLE_PRODUCT;
+    }
+
 }
