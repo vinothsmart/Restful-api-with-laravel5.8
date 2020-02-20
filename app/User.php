e6cf1116242fc6e793e26bfc2403d15e9ebb0a08
vinothsmart <s.vinothkanna90@gmail.com>
2020-02-20 13:24:55 +0530

    properties added

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Str;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,13 +11,24 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const VERIFIED_USER ='1';
+    const UNVERIFIED_USER ='0';
+
+    const ADMIN_USER = 'true';
+    const REGULAR_USER = 'false';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
+        'verified',
+        'verification_token',
+        'admin'
     ];
 
     /**
@@ -25,7 +37,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
+        'verification_token'
     ];
 
     /**
@@ -36,4 +50,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isVerified()
+    {
+        return $this->verified == User::VERIFIED_USER;
+    }
+
+    public function isAdmin()
+    {
+        return $this->admin == User::ADMIN_USER;
+    }
+
+    public function generateVerificationCode()
+    {
+        return Str::random(40);
+    }
+
 }
