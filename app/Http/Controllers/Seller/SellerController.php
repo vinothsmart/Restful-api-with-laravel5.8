360b981f75fe35ddac5032cc27a3249ebb20d883
vinothsmart <s.vinothkanna90@gmail.com>
2020-02-27 16:22:59 +0530

    all and view sellers

--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Seller;
 
-use App\Http\Controllers\Controller;
+use App\Seller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class SellerController extends Controller
 {
@@ -14,7 +15,9 @@ class SellerController extends Controller
      */
     public function index()
     {
-        //
+        $sellers = Seller::has('products')->get();
+
+        return response()->json(['data' => $sellers], 200);
     }
 
     /**
@@ -46,7 +49,9 @@ class SellerController extends Controller
      */
     public function show($id)
     {
-        //
+        $seller = Seller::has('products')->findOrFail($id);
+
+        return response()->json(['data' => $seller], 200);
     }
 
     /**
