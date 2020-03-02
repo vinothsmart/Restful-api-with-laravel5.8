6e60086a5a177af5a5547781c088d41d3e19e6ae
vinothsmart <s.vinothkanna90@gmail.com>
2020-03-02 17:15:17 +0530

    Seller Scope added

--- app/Buyer.php
+++ app/Buyer.php
@@ -10,11 +10,11 @@ class Buyer extends User
     protected static function boot()
     {
         parent::boot();
-        
+
         static::addGlobalScope(new BuyerScope);
     }
-    
-    
+
+
     public function transactions()
     {
         return $this->hasMany('App\Transaction');
