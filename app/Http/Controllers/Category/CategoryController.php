159d0260ad48295892d8b5b3faa80d7f238c6157
vinothsmart <s.vinothkanna90@gmail.com>
2020-03-02 18:25:44 +0530

    view cat

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -50,8 +50,7 @@ class CategoryController extends ApiController
      */
     public function show(Category $category)
     {
-        //
-        
+        return $this->showOne($category);
     }
 
     /**
