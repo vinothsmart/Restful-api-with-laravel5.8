df2dd90b8eee6d1e931eb3b7675e373f638af44a
vinothsmart <s.vinothkanna90@gmail.com>
2020-03-09 19:01:34 +0530

    sellers with categories

--- routes/api.php
+++ routes/api.php
@@ -59,6 +59,8 @@ Route::resource('sellers', 'Seller\SellerController',['only' => ['index', 'show'
 
 Route::resource('sellers.transactions', 'Seller\SellerTransactionController',['only' => ['index']]);
 
+Route::resource('sellers.categories', 'Seller\SellerCategoryController',['only' => ['index']]);
+
 /**
 * Transactions
 */
