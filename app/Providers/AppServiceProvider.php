0585915d8a9056412f0cad7fbb1c77e8925e4550
vinothsmart <s.vinothkanna90@gmail.com>
2020-03-16 18:54:07 +0530

    push mail if fails

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,16 +31,26 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        User::created(function($user) {
-            Mail::to($user)->send(new UserCreated($user));
+        User::created(function ($user)
+        {
+            retry(5, function () use ($user)
+            {
+                Mail::to($user)->send(new UserCreated($user));
+            }
+            , 100);
         });
 
-        User::updated(function($user) {
-            if($user->isDirty('email')){
-                Mail::to($user)->send(new UserMailChanged($user));
+        User::updated(function ($user)
+        {
+            if ($user->isDirty('email'))
+            {
+                retry(5, function () use ($user)
+                {
+                    Mail::to($user)->send(new UserMailChanged($user));
+                }
+                , 100);
             }
         });
-
 
         Product::updated(function ($product)
         {
