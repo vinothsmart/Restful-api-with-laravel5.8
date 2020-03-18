1e51945d389c47d6a571685b7b5771d20bd78e64
vinothsmart <s.vinothkanna90@gmail.com>
2020-03-18 19:22:36 +0530

    sortby added

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -24,6 +24,7 @@ trait ApiResponser
 
         $transformer = $collection->first()->transformer;
  
+        $collection = $this->sortData($collection);
         $collection = $this->transformData($collection, $transformer);
         
         return $this->successResponse($collection, $code);
@@ -49,6 +50,16 @@ trait ApiResponser
         $transformation = fractal($data, new $transformer);
         return $transformation->toArray();
     }
- 
+
+    protected function sortData(Collection $collection)
+    {
+        if(request()->has('sort_by')) {
+            $attribute = request()->sort_by;
+    
+            $collection = $collection->sortBy->$attribute;
+        }
+        
+        return $collection;
+    }
 }
 
