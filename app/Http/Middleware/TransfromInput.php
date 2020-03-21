b2da3555651e3dafaf4c1e3e9cc1317c575b3b52
vinothsmart <s.vinothkanna90@gmail.com>
2020-03-21 11:52:05 +0530

    validation error changed transformed name

--- app/Http/Middleware/TransfromInput.php
+++ app/Http/Middleware/TransfromInput.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Validation\ValidationException;
 
 class TransfromInput
 {
@@ -16,16 +17,34 @@ class TransfromInput
     public function handle($request, Closure $next, $transformer)
     {
         $transformedInput = [];
-      
-       // adding condition transform value to  Original value
-       // For example title = name
-       foreach ($request->request->all() as $input => $value) {
-           $transformedInput[$transformer::originalAttribute($input)] = $value;
-       }
- 
-       // Then going to replace the value
-       $request->replace($transformedInput);
-      
-       return $next($request);
+        
+        // adding condition transform value to  Orginal value
+        // For example title = name
+        foreach ($request->request->all() as $input => $value) {
+            $transformedInput[$transformer::originalAttribute($input)] = $value;
+        }
+
+        // Then going to replace the value
+        $request->replace($transformedInput);
+        
+        $response = $next($request);
+
+        if(isset($response->exception) && $response->exception instanceof ValidationException){
+            $data = $response->getData();
+
+            $transformedErrors = [];
+
+            foreach ($data->error as $field => $error) {
+                $transformedField =  $transformer::transformedAttribute($field);
+
+                $transformedErrors[$transformedField]= str_replace($field, $transformedField, $error);
+            }
+
+            $data->error = $transformedErrors;
+
+            $response->setData($data);
+        }
+
+        return $response;
     }
 }
