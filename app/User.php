ffff998876c7b41c97164f656457f3b5a331d2e2
vinothsmart <s.vinothkanna90@gmail.com>
2020-03-24 16:18:06 +0530

    for user model Laravel passport added

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 namespace App;
 
 use Illuminate\Support\Str;
+use Laravel\Passport\HasApiTokens;
 use App\Transformers\UserTransformer;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, HasApiTokens, SoftDeletes;
 
     const VERIFIED_USER = '1';
     const UNVERIFIED_USER = '0';
