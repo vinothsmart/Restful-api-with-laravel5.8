ad73d339e7e6c965e91e535102410fd581493c31
vinothsmart <s.vinothkanna90@gmail.com>
2020-03-27 15:51:36 +0530

    registering scope in seller product

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -12,6 +12,7 @@ class ProductCategoryController extends ApiController
     {
         $this->middleware('client.credentials')->only(['index']);
         $this->middleware('auth:api')->except(['index']);
+        $this->middleware('scope:manage-product');
     }
  
     /**
