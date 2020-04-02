bd270e6c7e5f0ad0aefeee626a412ae0f6a4ccc8
vinothsmart <s.vinothkanna90@gmail.com>
2020-04-02 15:30:53 +0530

    scope added in usercontroller

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,3 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/home/my-tokens', 'HomeController@getTokens')->name('personal-tokens');
 Route::get('/home/my-clients', 'HomeController@getClients')->name('personal-clients');
 Route::get('/home/authorized-clients', 'HomeController@getAuthorizedClients')->name('authorized-clients');
-
-
-Auth::routes([ 'register' => false ]);
-
-Route::get('/home', 'HomeController@index')->name('home');
