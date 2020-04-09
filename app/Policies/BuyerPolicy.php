c605a2a4ce489f2410b6e076fa978d5aecf14a1d
vinothsmart <s.vinothkanna90@gmail.com>
2020-04-09 14:00:45 +0530

    code up

--- app/Policies/BuyerPolicy.php
+++ app/Policies/BuyerPolicy.php
@@ -11,17 +11,6 @@ class BuyerPolicy
     use HandlesAuthorization;
 
     /**
-     * Determine whether the user can view any buyers.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can view the buyer.
      *
      * @param  \App\User  $user
@@ -30,31 +19,8 @@ class BuyerPolicy
      */
     public function view(User $user, Buyer $buyer)
     {
-        //
+        return $user->id == $buyer->id;
     }
-
-    /**
-     * Determine whether the user can create buyers.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    // public function create(User $user)
-    // {
-    //     //
-    // }
-
-    /**
-     * Determine whether the user can update the buyer.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Buyer  $buyer
-     * @return mixed
-     */
-    // public function update(User $user, Buyer $buyer)
-    // {
-    //     //
-    // }
 
     /**
      * Determine whether the user can delete the buyer.
@@ -65,30 +31,7 @@ class BuyerPolicy
      */
     public function purchase(User $user, Buyer $buyer)
     {
-        //
+        return $user->id == $buyer->id;
     }
-
-    /**
-     * Determine whether the user can restore the buyer.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Buyer  $buyer
-     * @return mixed
-     */
-    // public function restore(User $user, Buyer $buyer)
-    // {
-    //     //
-    // }
-
-    /**
-     * Determine whether the user can permanently delete the buyer.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Buyer  $buyer
-     * @return mixed
-     */
-    // public function forceDelete(User $user, Buyer $buyer)
-    // {
-    //     //
     // }
 }
